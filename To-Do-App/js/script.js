20dce42becbafd0f63e83dbb3b9c095208bb329d
Douglas Obara <douglasobara97@gmail.com>
2022-07-05 22:19:58 +0300

    add function to accept data store in the object and display data on browser

--- To-Do-App/js/script.js
+++ To-Do-App/js/script.js
@@ -1,5 +1,9 @@
 let form = document.getElementById("form");
 let msg = document.getElementById("msg");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let tasks = document.getElementById("tasks");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -14,5 +18,31 @@ const formValidation = () => {
   } else {
     console.log("success");
     msg.innerHTML = "";
+    acceptData();
   }
+};
+
+// accept and store data
+let data = {};
+
+// accept data and push it to the data object
+let acceptData = () => {
+  data["text"] = textInput.value;
+  data["date"] = dateInput.value;
+  data["description"] = textarea.value;
+  console.log(data);
+  createTasks();
+};
+
+let createTasks = () => {
+  tasks.innerHTML += `
+    <div>
+        <span class="fw-bold">${data.text}</span>
+        <span class="small text-secondary">${data.date}</span>
+        <p>${data.description}</p>
+        <span class="options">
+        <i class="fas fa-edit"></i>
+        <i class="fas fa-trash-alt"></i>
+        </span>
+    </div>`;
 };
